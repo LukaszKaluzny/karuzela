49a47fd12def501a026ff4de77dabbbe0d672780
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-16 16:32:35 +0200

    branch with master

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(function(){
-	//ten kod wykona się po załadowaniu DOM
+  var carouselList = $("#carousel ul");
+  function moveFirstSlide () {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    function changeSlide () {
+      carouselList.css({marginLeft:0});
+      setInterval(changeSlide, 3000);
+      carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    }
+  }
+  moveFirstSlide ();
 });
-var carouselList = $("#carousel ul");
-
-function moveFirstSlide () {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-	setInterval(changeSlide, 3000);
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-}
-moveFirstSlide ();
